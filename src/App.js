a30172d1e25411fc929a046610318e04ab94871f
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-05-30 11:36:45 +0200

    Display Movie List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-
+  const [movies, setMovies] = useState([{
+{
+    "Title": "Star Wars: Episode VIII - The Last Jedi",
+    "Year": "2017",
+    "imdbID": "tt2527336",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
+},
+{
+    "Title": "Rogue One: A Star Wars Story",
+    "Year": "2016",
+    "imdbID": "tt3748528",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode IX - The Rise of Skywalker",
+    "Year": "2019",
+    "imdbID": "tt2527338",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
+}]);
+  return <div>
+    <MovieList movies={movies} />
+  </div>
 };
 
 export default App;
